30b7921eb06bdf7e75bbf9e9976406a5183e3135
thiennn <nguyenngocthien7742@gmail.com>
2023-11-18 19:23:33 +0700

    add project

---
+++ nhap.php
@@ -0,0 +1,32 @@
+<!--
+    1/react: library
+    laravel: framework
+    framework tổng hợp gồm nhiều library để thực hiện công việc mới ra đc frame work
+
+    2/Triết lý Convention over configuration
+
+
+
+    3/Lavarel Artisan: là một CLI command-line interface (cmd) tích hợp sẵn trong frame work
+
+
+mở terminal ở project
+gõ php artisan serve mở port 8000
+
+gõ php artisan serv --port=xxxx
+
+tạo github
+repositories -> new đặt tên -> chọn public
+gõ git --version (nếu có git thì ok không thì phải cài)
+terminal
+
+gõ
+git init
+git add .
+git commit -m "First Commit"
+git branch -M main
+git remote add origin http://github.com/tên/response
+git push-u
+
+git config --global user.email"nguyenngocthien7742@gmail.com"
+-->
